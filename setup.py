314cf7e881570cba94f56476d46a3b5423723509
muZk <neeco.gmz@gmail.com>
2015-11-22 02:46:33 -0300

    updated setup.py descriptions

--- setup.py
+++ setup.py
@@ -2,17 +2,23 @@
 # -*- coding: UTF-8 -*-
 
 from setuptools import setup
+import moddump
 
 setup(name='moddump',
-      version='0.1',
-      description='Dota2 Reborn script to generate a JSON of your mod.',
-      url='https://github.com/muZk/modjson',
+      version=moddump.__version__,
+      description='Dota2 Reborn command line utility to generate a localized JSON of your mod.',
+      url='https://github.com/muZk/moddump',
       author='Nicolás Gómez',
       author_email='neeco.gmz@gmail.com',
       license='MIT',
       packages=['moddump'],
       scripts= ['bin/moddump'],
       data_files=[('locales', ['dota_english.json'])],
+    	classifiers = [
+          "License :: OSI Approved :: MIT License",
+          "Intended Audience :: Developers",
+          "Programming Language :: Python"
+          ],
       install_requires=[
       	'steamodd',
       ],
